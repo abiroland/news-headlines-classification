e86c48988d11aab81b49726226adc95e8bb24b66
Roland <ra0033a@american.edu>
2024-05-27 19:03:25 -0700

    update app file

--- app_over.py
+++ app_over.py
@@ -16,11 +16,25 @@ import pickle
 app=Flask(__name__)
 # process data
 from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf_vectorizer = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
+tfidf_vectorizer = pickle.load(open('transformer_model.pkl', 'rb'))
 
 
 ## load the model
 nlpmodel=pickle.load(open('ovr_model.pkl', 'rb'))
+
+def targetnames(result):
+    if result == 0:
+        return "Business"
+    elif result == 1:
+        return "Education"
+    elif result == 2:
+        return "Entertainment"
+    elif result == 3:
+        return "Sports"
+    elif result == 3:
+        return "Technology"
+    else:
+        return "Unknown"
 
 
 @app.route('/')
@@ -30,16 +44,20 @@ def home():
 @app.route('/predict_api', methods=['POST'])
 def predict_app():
     data=request.json['data']
-    new_data= tfidf_vectorizer.transform(data.values())
+    data = [data]
+    new_data= tfidf_vectorizer.transform(data) 
     output=nlpmodel.predict(new_data)
-    return jsonify(output.tolist())
+    return jsonify(targetnames(output))
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    if 'data' not in request.form:
+        return jsonify({'error': 'Missing data field'}), 400
     data=request.form['data']
-    new_data=tfidf_vectorizer.transform(data.values())
+    data = [data]
+    new_data=tfidf_vectorizer.transform(data)
     output=nlpmodel.predict(new_data)
-    return render_template('home.html', prediction = output.tolist())
+    return render_template('home.html', prediction = targetnames(output))
 
 if __name__ == '__main__':
     app.run(debug=True)
