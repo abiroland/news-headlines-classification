a1ae8f2b6005d4d068d871f09cedd49c275bcc12
Roland <ra0033a@american.edu>
2024-06-08 13:35:41 -0700

    updated app

--- app.py
+++ app.py
@@ -1,26 +1,20 @@
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV, cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+
 import json
 from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
 import pandas as pd
-import pickle
+import joblib
 
 
 
 app=Flask(__name__)
 # process data
 from sklearn.feature_extraction.text import TfidfVectorizer
-tfidf_vectorizer = pickle.load(open('transformer_model.pkl', 'rb'))
+tfidf_vectorizer = joblib.load('transformer_model.joblib')
 
 
 ## load the model
-nlpmodel=pickle.load(open('ovr_model.pkl', 'rb'))
+nlpmodel=joblib.load('ovr_model.joblib')
 
 def targetname(x):
     if x == 0:
@@ -44,16 +38,16 @@ def home():
 @app.route('/predict_api', methods=['POST'])
 def predict_app():
     data=request.json['data']
-    new_data= tfidf_vectorizer.transform(data.values()) 
+    new_data= tfidf_vectorizer.transform(data) 
     output=nlpmodel.predict(new_data)
-    return jsonify(targetname(output))
+    return jsonify(output.tolist())
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data=request.form['data']
-    new_data=tfidf_vectorizer.transform(data.values())
+    new_data=tfidf_vectorizer.transform(data)
     output=nlpmodel.predict(new_data)
-    return render_template('home.html', prediction = {targetname(output)})
+    return render_template('home.html', prediction = output.tolist())
 
 if __name__ == '__main__':
     app.run(debug=True)
